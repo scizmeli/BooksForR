01706ea2f2971ee8cc407dabb2b2c28555b19159
suleymantaspinar <suleyman.taspinar@protonmail.com>
2019-09-16 19:22:30 +0300

    parsed div elements

--- booksNameExtract.R
+++ booksNameExtract.R
@@ -3,58 +3,172 @@ library(httr)
 library(rvest)
 library(tidyverse)
 library(dplyr)
-library(XML)
-library(bookdown)
-
-render_book("r-pkgs/index.Rmd",output_format = )
 
 
-url <- GET("https://bookdown.org")
-  Table<-readHTMLList(rawToChar(url$content))
-tablex <- read_html(rawToChar(url$content))
+#Getting all href links in the li element.
+url <- GET("https://r-pkgs.org")
 
-
-
-
-
-
-booksAuthors <- function(){
+#This function scraps all chapter url and all subchapter urls.
+chapterLink <- function(){
+  page <- read_html(url)
+  Sys.sleep(50)
+  nodes <- html_nodes(page,'li.chapter a')
   
-  page <- read_html(url)  
-  
-  nodes <- html_nodes(page,'.article-list  h2')
-  gsub("by"," ",html_text(nodes))
-  
+  paste("https://r-pkgs.org/",html_attr(nodes,"href"),sep="")
 }
 
 
-
-booksName <- function(){
-  
-  page <- read_html(url)  
-  
-  nodes <- html_nodes(page,'.article-list  h1')
-  html_text(nodes)
-  
-  
-}
+#saving href link as data frame
+chapterURLS <- data.frame(links = chapterLink())
+anyNA(chapterURLS)
+chapterURLS[,1] <-as.character(chapterURLS[,1])
 
 
+#creating new column
+chapterURLS$ChapterName<- chapterURLS$links
 
 
-booksLinks <- function(){
+#urlFunc generates url for every part of book.
+urlFunc <- function(x){
+    page <- read_html(url)
+    #parsing the html for h1
+    node <- html_node(page,'h1')
 
-  page <- read_html(url)  
-
-  nodes <- html_nodes(page,'h1 a')
-
-  html_attr(nodes,"href")
+    html_text(node)
+    #chapterURLS[x,2] <- cleanText
 }
 
 
 
 
-df <- data.frame("Book"= booksName(),
-                 "Author"= booksAuthors(),
-                 "Link" = booksLinks())
+#saving the results 
+for(i in 1:length(chapterURLS[,1])){
+  chapterURLS[i,2] <- urlFunc(i)
+}
 
+
+#creating new column
+chapterURLS$Chapter <- chapterURLS$V2
+for(x in 1:length(chapterURLS[,1])){
+  ifelse ( !grepl( "#" , chapterURLS[x,1] ),
+           chapterURLS[x,3] <-"Chapter" ,
+           chapterURLS[x,3] <-"SubChapter" )
+}
+
+
+
+read_html(chapterURLS[x,1])
+
+
+#This function accepts urls and give the outputs as div element of links
+parseSubChapterElements <- function(x){
+  if( grepl( "#" , chapterURLS[x,1] )){
+    y <- regexpr( "\\#.*" , chapterURLS[x,1] )
+    regmatches( chapterURLS[x,1],y )
+    #read_html(page,paste())
+  }else{
+    warning("This url is not subchapter")
+  }
+}
+
+
+
+#This function accepts urls and give the outputs as div element of links 
+parseChapterElements <- function(x){
+  if( !grepl( "#" , chapterURLS[x,1] )){
+    y <- gsub("https://r-pkgs.org/","",chapterURLS[x,1])
+    y <- gsub(".html","",y)
+    y
+  }else{
+    warning("This url is  not chapter")
+  }
+}
+
+
+
+
+# #Subsetting only chapters 
+# OnlyChapter<-chapterURLS[chapterURLS$V3 == "Chapter",]
+# OnlyChapter$Text <- OnlyChapter$links
+# 
+# getChapterText <- function(x){
+#   url <- GET(OnlyChapter[x,1])
+#   Sys.sleep(55)
+#   page <- read_html(url)
+#   node <- html_node(page,'#section-')
+#   cleanText <- html_text(node)
+#   cleanText
+# }
+# 
+# 
+# 
+# 
+# for(x in 1:length(OnlyChapter[,1])){
+#   OnlyChapter[x,4] <- getChapterText(x)
+# #  OnlyChapter[x,5] <- getDirtyText(x)
+# }
+# 
+
+
+
+# xyz <- function(x){
+#   ort <- x+1
+#   lolo<- x+5
+#   return(data.frame("ort"=ort,
+#                     "lol"=lolo))
+# }
+# 
+# 
+# for(i in 1:5){
+#   xyz <- xyz(i)
+# }
+
+#Creating new column for 
+# OnlyChapter$DirtyText <- OnlyChapter$ChapterName
+
+
+# getDirtyText <- function(x){
+#   url <- GET(OnlyChapter[x,1])
+#   Sys.sleep(30)
+#   page <- read_html(url)
+#   node <- html_node(page,'#section-')
+#   node
+#   #cleanText <- html_text(node)
+#   #cleanText
+#   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for(x in 1:length(OnlyChapter[,1])){
+# 
+#   OnlyChapter[x,5] <- as.character(getDirtyText(x))
+#   
+# }
+# 
+
+
+
+
+
+
+#Searching for pattern 
+# x <- "aasd"
+# y <- "s"
+# grepl(y,x)
+
+
+
+save.image(file="environment")
