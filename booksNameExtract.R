71aa03556bcf81c8b3f431d5374b5a0acb973341
suleymantaspinar <suleyman.taspinar@protonmail.com>
2019-09-19 16:16:52 +0300

    "added some functionality"

--- booksNameExtract.R
+++ booksNameExtract.R
@@ -11,8 +11,10 @@ url <- GET("https://r-pkgs.org")
 #This function scraps all chapter url and all subchapter urls.
 chapterLink <- function(){
   page <- read_html(url)
+  
   Sys.sleep(50)
-  nodes <- html_nodes(page,'li.chapter a')
+ 
+   nodes <- html_nodes(page,'li.chapter a')
   
   paste("https://r-pkgs.org/",html_attr(nodes,"href"),sep="")
 }
@@ -30,14 +32,42 @@ chapterURLS$ChapterName<- chapterURLS$links
 
 #urlFunc generates url for every part of book.
 urlFunc <- function(x){
-    page <- read_html(url)
-    #parsing the html for h1
-    node <- html_node(page,'h1')
-
-    html_text(node)
-    #chapterURLS[x,2] <- cleanText
+  page <- read_html(url)
+  #parsing the html for h1
+  node <- html_node(page,'h1')
+  
+  html_text(node)
+  #chapterURLS[x,2] <- cleanText
 }
 
+
+
+
+
+#we can reach the all chapter names using any of the links
+page <- read_html(chapterURLS$links[5])
+
+#getting chapter names
+chapterNumbers <- html_text(html_nodes(page,'a'))
+
+
+#Deleting manually duplicated chapter[1:2] == "R packages"
+#chapterNumbers[1:2] <- NULL
+#233 is the length(ChapterURLS)
+chapterURLS$ChapterName <-chapterNumbers[2 : 234]
+
+
+
+
+
+for(i in 1:length(OnlyChapter$DirtyText)){
+  
+  page <-read_html(OnlyChapter$DirtyText[[1]])
+  html_nodes(page,'#.r')
+  html_text(page)
+  
+  node <- html_node
+}
 
 
 
@@ -49,6 +79,9 @@ for(i in 1:length(chapterURLS[,1])){
 
 #creating new column
 chapterURLS$Chapter <- chapterURLS$V2
+
+
+#assigning the chapter urls chapter or subchapter
 for(x in 1:length(chapterURLS[,1])){
   ifelse ( !grepl( "#" , chapterURLS[x,1] ),
            chapterURLS[x,3] <-"Chapter" ,
@@ -57,10 +90,84 @@ for(x in 1:length(chapterURLS[,1])){
 
 
 
-read_html(chapterURLS[x,1])
+
 
 
 #This function accepts urls and give the outputs as div element of links
+parseSubChapterElements <- function(x){
+  if( grepl( "#" , chapterURLS[x,1] )){
+    y <- regexpr( "\\#.*" , chapterURLS[8,1] )
+    div <- regmatches( chapterURLS[8,1],y )
+    #page <- read_html(chapterURLS[8,1])
+    #print("some sleep" , Sys.sleep(8))
+    #node <- html_node(page, div)
+    
+    
+    if(is.null(grepl("sourceCode r", node)))    
+      #read_html(page,paste())
+  }else{
+    warning("This url is not subchapter")
+  }
+}
+
+
+
+#This function accepts urls and give the outputs as div element of links 
+parseChapterElements <- function(x){
+  if( !grepl( "#" , chapterURLS[x,1] )){
+    y <- gsub("https://r-pkgs.org/","",chapterURLS[x,1])
+    div <- gsub(".html","",y)
+    
+    
+  }else{
+    warning("This url is  not chapter")
+  }
+}
+
+
+
+#Subsetting only chapters for after use if needed.
+OnlyChapter<-chapterURLS[chapterURLS$V3 == "Chapter",]
+OnlyChapter$Text <- OnlyChapter$links
+
+
+#Parsing all chapters texts(both text and code)
+getChapterText <- function(x){
+  url <- GET(OnlyChapter[x,1])
+  Sys.sleep(55)
+  page <- read_html(url)
+  node <- html_node(page,'#section-')
+  cleanText <- html_text(node)
+  cleanText
+}
+for(x in 1:length(OnlyChapter[,1])){
+  OnlyChapter[x,4] <- getChapterText(x)
+  #  OnlyChapter[x,5] <- getDirtyText(x)
+}
+
+
+
+
+
+
+
+#Creating new column for all chapters dirty html texts
+OnlyChapter$DirtyText <- OnlyChapter$ChapterName
+getDirtyText <- function(x){
+  url <- GET(OnlyChapter[x,1])
+  Sys.sleep(30)
+  page <- read_html(url)
+  node <- html_node(page,'#section-')
+  node
+}
+for(x in 1:length(OnlyChapter[,1])){
+  
+  OnlyChapter[x,5] <- as.character(getDirtyText(x))
+  
+}
+
+
+
 parseSubChapterElements <- function(x){
   if( grepl( "#" , chapterURLS[x,1] )){
     y <- regexpr( "\\#.*" , chapterURLS[x,1] )
@@ -70,7 +177,6 @@ parseSubChapterElements <- function(x){
     warning("This url is not subchapter")
   }
 }
-
 
 
 #This function accepts urls and give the outputs as div element of links 
@@ -85,90 +191,62 @@ parseChapterElements <- function(x){
 }
 
 
+parseChapterElements(1)
+read_html()
 
-
-# #Subsetting only chapters 
-# OnlyChapter<-chapterURLS[chapterURLS$V3 == "Chapter",]
-# OnlyChapter$Text <- OnlyChapter$links
-# 
-# getChapterText <- function(x){
-#   url <- GET(OnlyChapter[x,1])
-#   Sys.sleep(55)
-#   page <- read_html(url)
-#   node <- html_node(page,'#section-')
-#   cleanText <- html_text(node)
-#   cleanText
-# }
-# 
-# 
-# 
-# 
-# for(x in 1:length(OnlyChapter[,1])){
-#   OnlyChapter[x,4] <- getChapterText(x)
-# #  OnlyChapter[x,5] <- getDirtyText(x)
-# }
-# 
+#Searching for pattern
+x <- "aasd"
+y <- "s"
+grepl(y,x)
 
 
 
-# xyz <- function(x){
-#   ort <- x+1
-#   lolo<- x+5
-#   return(data.frame("ort"=ort,
-#                     "lol"=lolo))
-# }
-# 
-# 
-# for(i in 1:5){
-#   xyz <- xyz(i)
-# }
-
-#Creating new column for 
-# OnlyChapter$DirtyText <- OnlyChapter$ChapterName
+html_tag(page,'#intro')
 
 
-# getDirtyText <- function(x){
-#   url <- GET(OnlyChapter[x,1])
-#   Sys.sleep(30)
-#   page <- read_html(url)
-#   node <- html_node(page,'#section-')
-#   node
-#   #cleanText <- html_text(node)
-#   #cleanText
+parseSubChapterElements(5)
+
+
+attributes(node)
+
+
+extracingSubChapters <- function(){
+if(chapterURLS[x,3] == "Subchapter"){
+  page <- read_html(chapterURLS[x,1])
+
+  awsd1 <- html_node(page,'#intro-outline' ) 
+
+  for(x in 1:length(xml_parents(awsd1))){
+    #Something recursive 
+    #code gorunceye kadar devam et
+    #textleri birleştir
+    #code kısmına geçtiginde buraya kadar birlestirdgin textleri json olarak export et
+    #code kısmını export ettigin json cellinin altına ekle
+    #tekrar text görürsen aynı json cellinin altına devam et
+      }  
+  }
+  
+
+}
+
+
+
+
+
+# for(nodes in page){
+#     print(html_node(page,'#intro-outline' ))
 #   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for(x in 1:length(OnlyChapter[,1])){
 # 
-#   OnlyChapter[x,5] <- as.character(getDirtyText(x))
+# for(i in 1:length( OnlyChapter$links )){
 #   
+#   page <- read_html( OnlyChapter$DirtyText[[i]] )
+#   
+#   print(is.null(html_nodes(page,'.r'))) 
+#   html_text(html_nodes(page,'.r'))
+#   ax <- html_nodes(page,'.r')
+#   ax[1]
 # }
-# 
 
 
-
-
-
-
-#Searching for pattern 
-# x <- "aasd"
-# y <- "s"
-# grepl(y,x)
-
-
-
-save.image(file="environment")
+save.image(file="environment1")
